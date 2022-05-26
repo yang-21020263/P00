04d1ea0893c7b307d05335ebdd936ec7f8f99541
yang-21020263 <21020263@myrp.edu.sg>
2022-05-26 15:03:26 +0800

    initial commit in class

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -16,15 +16,61 @@ public class CalculatorTest {
 
 	@Test
 	public void testAdd() {
-		//
+		//Arrange
 		int a = 4321;
 		int b = 1234;
 		Calculator cal = new Calculator();
 		
-		//
+		//Act
 		int actual = cal.add(a, b);
 		
+		// Assert
 		int expected = 5555;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testSubtract() {
+		//Arrange
+		int a = 4321;
+		int b = 3210;
+		Calculator cal = new Calculator();
+		
+		// Act
+		int actual = cal.subtract(a, b);
+		
+		// Assert
+		int expected = 1111;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testMultiply() {
+		// Arrange
+		int a = 100;
+		int b = 10;
+		Calculator cal = new Calculator();
+		
+		// Act
+		int actual = cal.multiply(a, b);
+		
+		// Assert
+		int expected = 1000;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testDivide() {
+		// Arrange
+		int a = 100;
+		int b = 10;
+		Calculator cal = new Calculator();
+		
+		// Act
+		int actual = cal.divide(a, b);
+		
+		//Assert
+		int expected = 10;
 		assertEquals(expected, actual);
 	}
 
